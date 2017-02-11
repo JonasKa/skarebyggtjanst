15a143a056cff97d8beaba468f7d34601d7f6a59
Jonas Karlsson <jonas.m.karlsson@gmail.com>
2017-02-11 20:27:25 +0100

    More info

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.less']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('navbarToggler') navbarToggler:ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  navBarTogglerIsVisible() {
+    return this.navbarToggler.nativeElement.offsetParent !== null;
+  }
+
+  collapseNav() {
+    if (this.navBarTogglerIsVisible()) {
+      console.log("ska collapsa!");
+      this.navbarToggler.nativeElement.click();
+    }
   }
 
 }
